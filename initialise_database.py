a3ad2bdb7e031fd145857eb59e270adc6b42fdd2
Wheatley <30753183+hanlim83@users.noreply.github.com>
2022-05-28 15:23:48 +0000

    API Skeletion Completed

--- initialise_database.py
+++ initialise_database.py
@@ -8,11 +8,11 @@ with open('schema.sql') as f:
     connection.executescript(f.read())
 
 df = pd.read_csv('ramen-ratings.csv', dtype= {'ID': np.int32, 'Country': str, 'Brand': str, 'Type': str, 'Package': str, 'Rating': np.float64} ,na_values=['#VALUE!', ''])
-
 df.dropna(how='any', inplace=True)
+
 cur = connection.cursor()
 for i, row in df.iterrows():
     print(str(i) + ' - ' + str(row['ID']) + '  ' + row['Country'] + '  ' + row['Brand'] + '  ' + row['Type'] + '  ' + row['Package'] + '  ' + str(row['Rating']))
-    cur.execute("INSERT INTO reviews (ExID, Country, Brand, Type, Packaging, Rating) VALUES (?, ?, ?, ?, ?, ?)", (row['ID'], row['Country'], row['Brand'], row['Type'], row['Package'], row['Rating']))
+    cur.execute("INSERT INTO reviews (ExID, Country, Brand, Type, Package, Rating) VALUES (?, ?, ?, ?, ?, ?)", (row['ID'], row['Country'], row['Brand'], row['Type'], row['Package'], row['Rating']))
 connection.commit()
 connection.close()
